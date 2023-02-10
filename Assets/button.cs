9762cd42f045c2e371f6dcb9e385799c88defd4e
YanaChen424 <yanak424@gmail.com>
2023-02-10 22:35:58 +0200

    Implement steps 12 and 13

---
+++ Assets/button.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class button : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public bool IsSelected { get; private set; } = false;
+    // Start is called before the first frame update
+    public void OnSelect(BaseEventData data)
+    {
+        IsSelected = true;
+        GameManager.Instance.towerNameButton=gameObject.name;
+    }
+
+    public void OnDeselect(BaseEventData data)
+    {
+        IsSelected = false;
+    }
+}
