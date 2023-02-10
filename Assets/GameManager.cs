6546a1a90bd3420c7ac1adffb375e6bc2546316b
YanaChen424 <121768019+YanaChen424@users.noreply.github.com>
2023-02-10 22:46:11 +0200

    Implement steps 12 and 13 (#2)

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,6 +9,9 @@ public class GameManager : MonoBehaviour
 {
     public GameObject Tower;
 
+    public string towerNameButton;
+
+
     public static GameManager _instance;
     public static GameManager Instance
     {
@@ -20,6 +23,7 @@ public class GameManager : MonoBehaviour
     }
 
     public List<GameObject> enemyList = new List<GameObject> ();
+    public List<GameObject> towerList = new List<GameObject>(3);
 
 
     //PlayerNavMesh enemyIsDead;
@@ -50,30 +54,41 @@ public class GameManager : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitPoint;
-            if (Physics.Raycast(ray, out hitPoint))
+            if (Physics.Raycast(ray, out hitPoint)&&hitPoint.collider.CompareTag("placement"))
             {
-                if (bankAccountCalc >= 5)
+                if (towerList[0].name == towerNameButton)
                 {
-                    Instantiate(Tower, hitPoint.point, Tower.transform.localRotation);
-                    bankAccountCalc -= 5;
-                    bankAccount.text = bankAccountCalc.ToString();
+                    if (bankAccountCalc >= 2)
+                    {
+                        Instantiate(towerList[0], hitPoint.point, towerList[0].transform.localRotation);
+                        bankAccountCalc -= 2;
+                        bankAccount.text = bankAccountCalc.ToString();
+                    }
+                }
+                else if (towerList[1].name == towerNameButton)
+                {
+                    if (bankAccountCalc >= 4)
+                    {
+                        Instantiate(towerList[1], hitPoint.point, towerList[1].transform.localRotation);
+                        bankAccountCalc -= 4;
+                        bankAccount.text = bankAccountCalc.ToString();
+                    }
+                }
+                else if (towerList[2].name == towerNameButton)
+                {
+                    if (bankAccountCalc >= 5)
+                    {
+                        Instantiate(towerList[2], hitPoint.point, towerList[2].transform.localRotation);
+                        bankAccountCalc -= 5;
+                        bankAccount.text = bankAccountCalc.ToString();
+                    }
                 }
             }
         }
-        //for(int i = 0; i < enemyList.Count; i++)
-        //{
-        //    GameObject enemy = enemyList[i];
-        //    enemyIsDead = enemy.GetComponent<PlayerNavMesh>();
-        //    if (enemyIsDead.enemyIsDead)
-        //    {
-        //        print(enemyIsDead.enemyIsDead);
-        //        enemyCount++;
-        //        bankAccountCalc += 2;
-        //        bankAccount.text=bankAccountCalc.ToString();
-        //        //towerCount++;
-        //        enemyIsDead.enemyIsDead = false;
-        //    }
-        //}
+        if (towerNameButton != null)
+        {
+            print(towerNameButton);
+        }
 
     }
     public void OnEnemyDead(GameObject enemy) // looks good! :)
